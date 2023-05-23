5c456d69d0a5e0386cd9fe63169fc878217bf541
alecfong1 <alecfong1@gmail.com>
2023-05-23 22:55:01 +0000

    add cog build

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -1,1 +1,3 @@
 sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"\nsudo chmod +x /usr/local/bin/cog
+
+cd Efficient_RedPajama_Finetuning && cog build && cd -

0747a26beed5045308447d45cdbf49a1fdf6e79b
alecfong1 <alecfong1@gmail.com>
2023-06-16 00:21:48 +0000

    install latest drivers

--- .brev/setup.sh
+++ .brev/setup.sh
@@ -1,4 +1,8 @@
 sudo curl -o /usr/local/bin/cog -L "https://github.com/replicate/cog/releases/latest/download/cog_$(uname -s)_$(uname -m)"
 sudo chmod +x /usr/local/bin/cog
 
-cd Efficient_RedPajama_Finetuning && cog build && cd -
+wget https://developer.download.nvidia.com/compute/cuda/12.1.1/local_installers/cuda_12.1.1_530.30.02_linux.run
+sudo sh cuda_12.1.1_530.30.02_linux.run --silent
+rm cuda_12.1.1_530.30.02_linux.run
+
+# cd Efficient_RedPajama_Finetuning && cog build && cd -
